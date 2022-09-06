d85a2ebac4a7219cf74eb802de2bc1982d86c682
Nick <nwdepatie@gmail.com>
2022-09-06 11:11:15 -0400

    Torque limit set to 2220

--- MPU/include/pedals.h
+++ MPU/include/pedals.h
@@ -20,7 +20,7 @@
 #define BRAKELIGHT_PIN              4
 
 // motor torque constants
-#define MAXIMUM_TORQUE              222    // in Nm x 10 (ex: 123 = 12.3Nm)
+#define MAXIMUM_TORQUE              2220    // in Nm x 10 (ex: 123 = 12.3Nm)
 #define CONT_TORQUE                 1020    // ' '
 #define POT_LOWER_BOUND             35      // a pot value from 0 to 1023
 #define POT_UPPER_BOUND             MAX_ADC_VALUE    // a pot value from 0 to 1023
